d2a599e833ee0acf447ae3577754e869c52cbd72
EGeezzer <146590915+EGeezzer@users.noreply.github.com>
2025-09-28 22:16:16 -0400

    Update EmpTiemComp.java

--- Practica 4/Ejercicio 1/EmpTiemComp.java
+++ Practica 4/Ejercicio 1/EmpTiemComp.java
@@ -1,14 +1,21 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Practica_4;
+public class EmpTiemComp extends Empleado {
+    private double salarioAnual;
 
-/**
- *
- * @author PC
- */
-public class EmpTiemComp {
-    
+    public EmpTiemComp(String nombre, double salarioAnual) {
+        super(nombre);
+        this.salarioAnual = salarioAnual;
+    }
+    @Override
+    public double CalcularSalarioMensual() {
+        return salarioAnual / 12;
+    }
+    @Override
+    public String toString() {
+        return super.toString() + 
+               ", Salario Anual: " + salarioAnual +
+               ", Salario Mensual: " + CalcularSalarioMensual();
+    }
 }
+
+ 
