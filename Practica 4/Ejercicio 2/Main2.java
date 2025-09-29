d6a7b194d0c16d2be102f4ff3a1c180fee5e5582
EGeezzer <146590915+EGeezzer@users.noreply.github.com>
2025-09-28 22:18:13 -0400

    Update Main2.java

--- Practica 4/Ejercicio 2/Main2.java
+++ Practica 4/Ejercicio 2/Main2.java
@@ -1,14 +1,31 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Practica_4;
-
-/**
- *
- * @author PC
- */
+import java.util.Random;
 public class Main2 {
-    
+    public static void main(String[] args) {
+        Random rand = new Random();
+        Figura[] figuras = new Figura[5];
+        // Crear 5 figuras aleatorias
+        for (int i = 0; i < figuras.length; i++) {
+            int tipo = rand.nextInt(2); // 0 = Cuadrado, 1 = Círculo
+            if (tipo == 0) {
+                double lado = 1 + rand.nextInt(10);
+                figuras[i] = new Cuadrado("Rojo", lado);
+            } else {
+                double radio = 1 + rand.nextInt(10);
+                figuras[i] = new Circulo("Azul", radio);
+            }
+        }
+        // Mostrar resultados
+        for (Figura f : figuras) {
+            System.out.println(f.toString());
+            if (f instanceof Coloreado) {
+                Coloreado c = (Coloreado) f;
+                System.out.println("Método comoColorear: " + c.comoColorear());
+            }
+            System.out.println();
+        }
+    }
 }
+
+
+
