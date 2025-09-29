674795ac95350a4cd4051e21853a9ee9abcad5cb
EGeezzer <146590915+EGeezzer@users.noreply.github.com>
2025-09-28 22:17:18 -0400

    Update Circulo.java

--- Practica 4/Ejercicio 2/Circulo.java
+++ Practica 4/Ejercicio 2/Circulo.java
@@ -1,14 +1,24 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Practica_4;
+public class Circulo extends Figura {
+    private double radio;
 
-/**
- *
- * @author PC
- */
-public class Circulo {
-    
+    public Circulo(String color, double radio) {
+        super(color);
+        this.radio = radio;
+    }
+    @Override
+    public double area() {
+        return Math.PI * radio * radio;
+    }
+    @Override
+    public double perimetro() {
+        return 2 * Math.PI * radio;
+    }
+    @Override
+    public String toString() {
+        return "Círculo - " + super.toString() + 
+               ", Radio: " + radio + 
+               ", Área: " + area() + 
+               ", Perímetro: " + perimetro();
+    }
 }
