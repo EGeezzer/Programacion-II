5dcb2d0e7274a57274e6d3e9b7d117a4d4f44705
EGeezzer <146590915+EGeezzer@users.noreply.github.com>
2025-09-28 22:17:35 -0400

    Update Coloreado.java

--- Practica 4/Ejercicio 2/Coloreado.java
+++ Practica 4/Ejercicio 2/Coloreado.java
@@ -1,14 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Practica_4;
-
-/**
- *
- * @author PC
- */
-public class Coloreado {
-    
+public interface Coloreado {
+    String comoColorear();
 }
